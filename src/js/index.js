0542c9545dc53fd8e23e9eb177f8ce3c9c1513ae
vikash chauhan <vikash.chauhan@hashedin.com>
2020-08-15 23:07:12 +0530

    recipe controller, ingredient list model and controller

--- src/js/index.js
+++ src/js/index.js
@@ -43,3 +43,77 @@ elements.searchForm.addEventListener('submit', (e) => {
 	e.preventDefault();
 	searchHandler();
 });
+
+// pagination
+elements.searchResPages.addEventListener('click', (e) => {
+	const btnElement = e.target.closest('.btn-inline');
+	if (btnElement) {
+		const renderPage = parseInt(btnElement.dataset.goto, 10);
+		searchView.clearResults();
+		searchView.renderResults(state.search.recipes, renderPage);
+	}
+});
+
+// recipe controller
+const recipeController = async () => {
+	const recipeId = window.location.hash.replace('#', '');
+
+	if (recipeId) {
+		recipeView.clearRecipe();
+		renderLoader(elements.recipe);
+		if (state.search) {
+			searchView.highlightSelected(recipeId);
+		}
+		state.recipe = new Recipe(recipeId);
+		try {
+			await state.recipe.getIndividualRecipe();
+			state.recipe.convertIngredients();
+			clearLoader();
+			recipeView.renderRecipe(state.recipe, false);
+		} catch (error) {
+			alert('Error occurred while fetching Recipe');
+			console.log('Recipe Error: ', error);
+		}
+	}
+};
+
+window.addEventListener('hashchange', recipeController);
+window.addEventListener('load', recipeController);
+
+// increase/decrease functionality
+elements.recipe.addEventListener('click', (e) => {
+	if (e.target.matches('.btn-increase, .btn-increase *')) {
+		state.recipe.updateServings('increase');
+		recipeView.updateServingsIngredients(state.recipe);
+	} else if (e.target.matches('.btn-decrease, .btn-decrease *')) {
+		if (state.recipe.servings > 1) {
+			state.recipe.updateServings('decrease');
+			recipeView.updateServingsIngredients(state.recipe);
+		}
+	} else if (e.target.matches('.recipe__btn--add, .recipe__btn--add *')) {
+		listController();
+	}
+});
+
+const listController = () => {
+	if (!state.list) {
+		state.list = new List();
+	}
+	state.recipe.ingredients.forEach((ingItem) => {
+		const item = state.list.addIngItem(
+			ingItem.count,
+			ingItem.unit,
+			ingItem.ingredient
+		);
+		listView.renderItem(item);
+	});
+};
+
+// delete ingredient items
+elements.shopping.addEventListener('click', (e) => {
+	const id = e.target.closest('.shopping__item').dataset.itemid;
+	if (e.target.matches('.shopping__delete, .shopping__delete *')) {
+		state.list.deleteIngItem(id);
+		listView.deleteItem(id);
+	}
+});
--- src/js/models/List.js
+++ src/js/models/List.js
@@ -1,3 +1,22 @@
-
+import uniqid from 'uniqid';
 export default class List {
+	constructor() {
+		this.ingItems = [];
+	}
+
+	addIngItem(count, unit, ingredient) {
+		const ingItem = {
+			id: uniqid(),
+			count,
+			unit,
+			ingredient
+		};
+		this.ingItems.push(ingItem);
+		return ingItem;
+	}
+
+	deleteIngItem(id) {
+		const index = this.ingItems.findIndex((ingItem) => ingItem.id === id);
+		this.ingItems.splice(index, 1);
+	}
 }
